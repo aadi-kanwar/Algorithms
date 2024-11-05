7ed7c106ba5f56f889f22b4d765cc93c5d6e2fb4
Aadi Kanwar <aadi.kanwar@gmail.com>
2024-11-05 11:46:16 +0530

    Day 11

---
+++ Day 11/P1.pl
@@ -0,0 +1,17 @@
+valuable(gold) .
+raining(yesterday).
+eat(today).
+playing(tomorrow).
+fighting(people).
+hunting(animals).
+colorful(rainbow).
+colorful(people).
+coding(developer).
+snow(skiing).
+climbing(hill).
+flowing(river).
+tall(building).
+
+
+doctor(adult, men, women).
+
---
+++ Day 11/TT.pl
@@ -0,0 +1,5 @@
+mon(fr,cn,os,lunch).
+tue(lct,lct,ai,ai,os).
+wed(os,fr,fr,cn).
+thu(os,os,fr,nndl).
+fri(ml,nndle,nndl,toc).
